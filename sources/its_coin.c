e627e7c9240103e2e80d44e0c65ad61ad8a2257f
ThEWoLf882-42 <agimi@student.1337.ma>
2023-02-17 13:02:34 +0100

    the ultimate coinzzzzzz

--- sources/its_coin.c
+++ sources/its_coin.c
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 18:34:00 by agimi             #+#    #+#             */
-/*   Updated: 2023/02/16 10:19:35 by agimi            ###   ########.fr       */
+/*   Updated: 2023/02/17 13:02:04 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,18 @@ void	its_coin(t_all *all)
 	t = all->codb->next;
 	while (t)
 	{
-		if (all->px == (t->cx * 69) && all->py + 27 == (t->cy * 69))
+		if (all->px >= (t->cx * 69) - 13 && all->px <= (t->cx * 69) + 69 - 13)
 		{
-			mlx_image_to_window(all->mlx, all->grass.grass, \
+			if (all->py + 27 >= (t->cy * 69) - 22 && all->py + 27 <= (t->cy * 69) - 22 + 69)
+			{
+				mlx_image_to_window(all->mlx, all->grass.grass, \
 				(t->cx * 69), (t->cy * 69));
-			mlx_image_to_window(all->mlx, all->player.player, \
-				(t->cx * 69), (t->cy * 69));
-			all->coin.cc++;
-			t->cx = -1;
-			t->cy = -1;
+				mlx_image_to_window(all->mlx, all->player.player, \
+				all->px, all->py);
+				all->coin.cc++;
+				t->cx = -1;
+				t->cy = -1;
+			}
 		}
 		t = t->next;
 	}
